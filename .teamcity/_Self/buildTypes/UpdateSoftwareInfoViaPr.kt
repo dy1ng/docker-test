49179b3f9e95c89cd950b15ed5108cf33f552e62
mikhail.efremov <mikhail.efremov@TeamCity>
2021-07-09 19:20:52 +0300

    TeamCity change in 'Test project' project: runners of 'Update Software Info via PR' build configuration were updated

--- .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
+++ .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
@@ -1,6 +1,7 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.python
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object UpdateSoftwareInfoViaPr : BuildType({
@@ -66,14 +67,48 @@ object UpdateSoftwareInfoViaPr : BuildType({
                 curl -u dy1ng:%github_token% -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/dy1ng/teamcity-documentation/pulls -d "${'$'}json"
             """.trimIndent()
         }
-        script {
-            name = "Find and close old PRs"
-            scriptContent = """
-                #curl -u dy1ng:%github_token% -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/dy1ng/teamcity-documentation/pulls -d "${'$'}json"
-                
-                #Get list of PRs
-                PRS=${'$'}(curl -u dy1ng:%github_token% -X GET -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/dy1ng/teamcity-documentation/pulls)
-            """.trimIndent()
+        python {
+            name = "Cleanup old PRs and patch branches"
+            environment = venv {
+            }
+            command = script {
+                content = """
+                    from github import Github, UnknownObjectException
+                    
+                    
+                    def delete_branch(branch_name, rep):
+                        try:
+                            ref = rep.get_git_ref(f"heads/{branch_name}")
+                            ref.delete()
+                        except UnknownObjectException:
+                            print('No such branch', branch_name)
+                    
+                    
+                    if __name__ == '__main__':
+                        pr_list = []
+                        branch_list = {}
+                        g = Github("%github_token%")
+                        docs_repo = g.get_repo("dy1ng/teamcity-documentation")
+                        all_open_prs = docs_repo.get_pulls(state='open')
+                        # Filter PRs by login and name of head branches
+                        for pr in all_open_prs:
+                            if pr.user.login == g.get_user().login and 'patch-' in pr.head.ref:
+                                pr_list.append(pr)
+                        # Sort the list with PRs by create date and pop the most recent item
+                        pr_list.sort(key=lambda x: x.created_at, reverse=True)
+                        pr_list.pop(0)
+                        for pr in pr_list:
+                            print("Closing the following PR as obsolete:")
+                            print(f"\tTitle: '{pr.title}'; URL: '{pr.html_url}'; Created at: '{pr.created_at}'")
+                            pr.create_issue_comment("Closing this PR as obsolete, there is a more recent one")
+                            pr.edit(state='closed')
+                            # Store to-be-deleted branches that were used has head in PRs we just closed
+                            branch_list[pr.head.repo] = pr.head.ref
+                        for repo, branch in branch_list.items():
+                            print(f"Removing branch '{branch}' in repository '{repo.full_name}'")
+                            delete_branch(branch, repo)
+                """.trimIndent()
+            }
         }
     }
 
