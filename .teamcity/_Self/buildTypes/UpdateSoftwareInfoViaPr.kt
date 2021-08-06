f5e205fa71df0abe623d3e319af3f4736a7a0fc7
mikhail.efremov <mikhail.efremov@TeamCity>
2021-08-06 18:34:17 +0300

    TeamCity change in 'Test project' project: parameters of 'Update Software Info via PR' build configuration were updated

--- .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
+++ .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
@@ -9,6 +9,7 @@ object UpdateSoftwareInfoViaPr : BuildType({
 
     params {
         password("hiddenToken", "credentialsJSON:64b6255c-89a5-4f74-add0-402ed09ee64b", display = ParameterDisplay.HIDDEN, readOnly = true)
+        param("teamcity.cloud.documentation.branch.name.prefix", "dev-patch-")
         password("github_token", "credentialsJSON:c7d9a019-4a0e-4062-8f4c-467c55d64f54", display = ParameterDisplay.HIDDEN)
     }
 
