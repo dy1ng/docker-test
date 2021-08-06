5926f17da778bf35611891914b7137e35b302e93
mikhail.efremov <mikhail.efremov@TeamCity>
2021-08-06 18:38:39 +0300

    TeamCity change in 'Test project' project: runners of 'Update Software Info via PR' build configuration were updated

--- .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
+++ .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
@@ -44,8 +44,8 @@ object UpdateSoftwareInfoViaPr : BuildType({
                 git config user.name %teamcity.cloud.documentation.git.user.name%
                 git config user.email %teamcity.cloud.documentation.git.user.email%
                 
-                remotes=${'$'}{'${'$'}'}(git remote -v | grep "https://github.com/%teamcity.cloud.documentation.repo_name%.git" | wc -l)
-                if [[ ${'$'}{'${'$'}'}remotes -eq 0 ]] 
+                remotes=${'$'}(git remote -v | grep "https://github.com/%teamcity.cloud.documentation.repo_name%.git" | wc -l)
+                if [[ ${'$'}remotes -eq 0 ]] 
                 then
                     echo "######### Add upstream remote"
                 	exec git remote add upstream https://github.com/%teamcity.cloud.documentation.repo_name%.git
@@ -54,61 +54,61 @@ object UpdateSoftwareInfoViaPr : BuildType({
                 fi
                 
                 echo "###### Fetch, checkout a new temp branch from upstream's HEAD branch."
-                repo_head=${'$'}{'${'$'}'}(git remote show upstream | awk '/HEAD branch/ {print ${'$'}{'${'$'}'}NF}')
+                repo_head=${'$'}(git remote show upstream | awk '/HEAD branch/ {print ${'$'}NF}')
                 
-                exec git fetch upstream ${'$'}{'${'$'}'}repo_head
-                exec git checkout -b %teamcity.cloud.documentation.branch.name.prefix%%teamcity.build.id% upstream/${'$'}{'${'$'}'}repo_head
+                exec git fetch upstream ${'$'}repo_head
+                exec git checkout -b %teamcity.cloud.documentation.branch.name.prefix%%teamcity.build.id% upstream/${'$'}repo_head
                 echo "######"
                 
-                header_ubuntu=${'$'}{'${'$'}'}(cat << EOM
+                header_ubuntu=${'$'}(cat << EOM
                 [//]: # (title: Preinstalled Software on TeamCity Cloud Ubuntu Agents)
                 [//]: # (auxiliary-id: Preinstalled Software on TeamCity Cloud Ubuntu Agents)
                 
                 <chunk id="ubuntu-jb-agents">
                 EOM
                 )
-                header_windows=${'$'}{'${'$'}'}(cat << EOM
+                header_windows=${'$'}(cat << EOM
                 [//]: # (title: Preinstalled Software on TeamCity Cloud Windows Agents)
                 [//]: # (auxiliary-id: Preinstalled Software on TeamCity Cloud Windows Agents)
                 
                 <chunk id="windows-jb-agents">
                 EOM
                 )
-                footer=${'$'}{'${'$'}'}(cat << EOM
+                footer=${'$'}(cat << EOM
                 </chunk> 
                 EOM
                 )
-                body_ubuntu=${'$'}{'${'$'}'}(cat ../scripts/ubuntu/software.report.md)
-                body_windows=${'$'}{'${'$'}'}(cat ../scripts/windows/software.report.md)
+                body_ubuntu=${'$'}(cat ../scripts/ubuntu/software.report.md)
+                body_windows=${'$'}(cat ../scripts/windows/software.report.md)
                 
                 echo "###### Write software reports files"
                 cat > topics/preinstalled-software-on-teamcity-cloud-ubuntu-agents.md << EOM
-                ${'$'}{'${'$'}'}header_ubuntu
+                ${'$'}header_ubuntu
                 
-                ${'$'}{'${'$'}'}body_ubuntu
+                ${'$'}body_ubuntu
                 
-                ${'$'}{'${'$'}'}footer
+                ${'$'}footer
                 EOM
                 cat > topics/preinstalled-software-on-teamcity-cloud-windows-agents.md << EOM
-                ${'$'}{'${'$'}'}header_windows
+                ${'$'}header_windows
                 
-                ${'$'}{'${'$'}'}body_windows
+                ${'$'}body_windows
                 
-                ${'$'}{'${'$'}'}footer
+                ${'$'}footer
                 EOM
                 echo "######"
                 
                 echo "###### Git add, commit, push temp branch to origin"
                 exec git add topics/preinstalled-software-on-teamcity-cloud-ubuntu-agents.md
                 exec git add topics/preinstalled-software-on-teamcity-cloud-windows-agents.md
-                exec git commit -m "${'$'}{'${'$'}'}commit_msg"
+                exec git commit -m "${'$'}commit_msg"
                 exec git push origin %teamcity.cloud.documentation.branch.name.prefix%%teamcity.build.id%
                 echo "######"
                 
-                json="{\"head\":\"%teamcity.cloud.documentation.branch.name.prefix%%teamcity.build.id%\", \"base\":\"${'$'}{'${'$'}'}repo_head\", \"body\":\"${'$'}{'${'$'}'}{pr_body_msg}\", \"title\":\"Update preinstalled software list for TCC agents\"}"
-                #echo "${'$'}{'${'$'}'}json"
+                json="{\"head\":\"%teamcity.cloud.documentation.branch.name.prefix%%teamcity.build.id%\", \"base\":\"${'$'}repo_head\", \"body\":\"${'$'}{pr_body_msg}\", \"title\":\"Update preinstalled software list for TCC agents\"}"
+                #echo "${'$'}json"
                 echo "###### Create PR in documentation repo form temp branch"
-                curl -u %teamcity.cloud.documentation.token% -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/%teamcity.cloud.documentation.repo_name%/pulls -d "${'$'}{'${'$'}'}json"
+                curl -u %teamcity.cloud.documentation.token% -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/%teamcity.cloud.documentation.repo_name%/pulls -d "${'$'}json"
                 echo "######"
             """.trimIndent()
         }
