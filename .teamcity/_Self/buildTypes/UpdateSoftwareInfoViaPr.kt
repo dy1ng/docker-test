eb11de538c4e4a4c95b37b426b0a472bd1e52e00
mikhail.efremov <mikhail.efremov@TeamCity>
2021-08-13 11:25:45 +0300

    TeamCity change in 'Test project' project: runners of 'Update Software Info via PR' build configuration were updated

--- .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
+++ .teamcity/_Self/buildTypes/UpdateSoftwareInfoViaPr.kt
@@ -111,7 +111,7 @@ object UpdateSoftwareInfoViaPr : BuildType({
                 echo "######"
                 
                 json="{\"head\":\"%teamcity.cloud.documentation.git.user.name%:%teamcity.cloud.documentation.branch.name.prefix%%teamcity.build.id%\", \"base\":\"${'$'}repo_head\", \"body\":\"${'$'}{pr_body_msg}\", \"title\":\"Update preinstalled software list for TCC agents\"}"
-                #echo "${'$'}json"
+                echo "${'$'}json"
                 echo "###### Create PR in documentation repo form temp branch"
                 curl --user "%teamcity.cloud.documentation.login%:%teamcity.cloud.documentation.token%" -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/%teamcity.cloud.documentation.repo_name%/pulls -d "${'$'}json"
                 echo "######"
