1172742332911d3e63a6c49d56c6734ad570fed8
mikhail.efremov <mikhail.efremov@TeamCity>
2021-08-29 11:33:00 +0300

    TeamCity change in 'Test project' project: 'CompositeConfig' build configuration was created

---
+++ .teamcity/_Self/buildTypes/CompositeConfig.kt
@@ -0,0 +1,13 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+object CompositeConfig : BuildType({
+    name = "CompositeConfig"
+
+    type = BuildTypeSettings.Type.COMPOSITE
+
+    vcs {
+        showDependenciesChanges = true
+    }
+})
