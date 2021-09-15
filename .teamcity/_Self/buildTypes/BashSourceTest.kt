21ebc3268d6ca0811367da96b4414ff274bc2479
mikhail.efremov <mikhail.efremov@TeamCity>
2021-09-15 18:06:32 +0300

    TeamCity change in 'Test project' project: runners of 'Bash Source test' build configuration were updated

--- .teamcity/_Self/buildTypes/BashSourceTest.kt
+++ .teamcity/_Self/buildTypes/BashSourceTest.kt
@@ -20,5 +20,12 @@ object BashSourceTest : BuildType({
             name = "source the script"
             scriptContent = "source source_test.sh"
         }
+        script {
+            name = "check parameters"
+            scriptContent = """
+                echo "This is the value of TC parameter: %env.TEST_PARAM%"
+                echo "This is the value of the corresponding environmental variable: ${'$'}TEST_PARAM"
+            """.trimIndent()
+        }
     }
 })
