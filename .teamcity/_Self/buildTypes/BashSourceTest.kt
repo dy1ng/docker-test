7e4211fa47ce2b3a44cd95eb1a190a82c4ed0399
mikhail.efremov <mikhail.efremov@TeamCity>
2021-09-15 18:04:44 +0300

    TeamCity change in 'Test project' project: runners of 'Bash Source test' build configuration were updated

--- .teamcity/_Self/buildTypes/BashSourceTest.kt
+++ .teamcity/_Self/buildTypes/BashSourceTest.kt
@@ -1,7 +1,20 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object BashSourceTest : BuildType({
     name = "Bash Source test"
+
+    steps {
+        script {
+            name = "create sh. script"
+            scriptContent = """
+                cat > source_test.sh << EOM
+                echo "setting env.TEST_PARAM via service messages"
+                echo "##teamcity[setParameter name='env.TEST_PARAM' value='SUCCESS']"
+                EOM
+            """.trimIndent()
+        }
+    }
 })
