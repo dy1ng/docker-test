e5703f5acf2e4b92e9e0a3d1d547eb1aab38eb49
mikhail.efremov <mikhail.efremov@TeamCity>
2021-12-17 16:01:40 +0300

    TeamCity change in 'Test project / Artifact Dependency Test' project: general settings of 'Resolve Artifacts' build configuration were updated

--- .teamcity/ArtifactDependencyTest/buildTypes/ArtifactDependencyTest_ResolveArtifacts.kt
+++ .teamcity/ArtifactDependencyTest/buildTypes/ArtifactDependencyTest_ResolveArtifacts.kt
@@ -5,6 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 object ArtifactDependencyTest_ResolveArtifacts : BuildType({
     name = "Resolve Artifacts"
 
+    artifactRules = "+:project/__init__.py => project/"
+
     dependencies {
         artifacts(ArtifactDependencyTest_DependentBuild) {
             buildRule = lastSuccessful("+:*")
