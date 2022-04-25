8a7d0b501a7fe2236e4cd1319c744b9403a98fd2
mikhail.efremov <mikhail.efremov@TeamCity>
2022-04-25 12:13:41 +0300

    TeamCity change in 'Test project' project: Cleanup old configuration files

--- .teamcity/ArtifactDependencyTest/buildTypes/ArtifactDependencyTest_GetArtifactsViaRest.kt
+++
@@ -1,7 +0,0 @@
-package ArtifactDependencyTest.buildTypes
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
-object ArtifactDependencyTest_GetArtifactsViaRest : BuildType({
-    name = "Get Artifacts via REST"
-})
